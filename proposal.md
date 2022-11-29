b377acdc31c6bdd70106e71ad31337375421085f
Kesur0403463 <0403463@sps-student.sk.ca>
2022-11-29 12:12:52 -0600

    Updated proposal

--- proposal.md
+++ proposal.md
@@ -1,13 +1,13 @@
 # Proposal
 
-Description of what I'm going to do
+I am going to recreate Pac-Man. 
 
 ## Needs to Have List
 
 - Pac-man movement(WASD)
 - Ghost AI different enough for four ghosts(AI to chase and run from Pac-man)
-- Food power-up
-- Can tell when all the pellets have been eaten
+- Power-up pellets
+- Can advance levels when conditions are met
 - Code sending back Ghosts after being eaten
 
 ## Nice to Have List
@@ -16,3 +16,10 @@ Description of what I'm going to do
 - Ghost animation(colour to blink faster when the power-up is running out)
 - Different levels
 - Code for letting one ghost out periodically
+- Ghost house reincarnates ghosts from eyeball form
+
+
+## Questions for chat
+
+- do you think animation is achieveable in a 2Darray?
+-
--- sketch copy.js
+++ sketch copy.js
@@ -2,7 +2,6 @@
 // Kati Kesur
 // Novmeber 17, 2022
 
-// used to code ghost movement https://www.todayifoundout.com/index.php/2015/10/ghosts-pac-man-work/#:~:text=Although%20the%20ghosts%20can%20sometimes,turn%20and%20then%20continue%20moving.
 
 let grid = [
   [B, B, B, B, B, B, B, B, B, B, B, B, B, B, B, B, B, B, B, B],
@@ -10,7 +9,7 @@ let grid = [
   [B, 0, B, B, 0, B, 0, B, B, B, B, B, B, 0, B, 0, B, B, 0, B],
   [B, 0, B, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, B, 0, B],
   [B, 0, B, 0, B, B, 0, B, B, 1, 1, B, B, 0, B, B, 0, B, 0, B],
-  [0, 0, 0, 0, 0, 0, 0, B, G, 1, 1, 1, B, 0, 0, 0, 0, 0, 0, 0],
+  [B, 0, 0, 0, 0, 0, 0, B, G, 1, 1, 1, B, 0, 0, 0, 0, 0, 0, B],
   [B, 0, B, 0, B, B, 0, B, B, B, B, B, B, 0, B, B, 0, B, 0, B],
   [B, 0, B, 0, 0, 0, 0, 0, 0, M, 0, 0, 0, 0, 0, 0, 0, B, 0, B],
   [B, 0, B, B, 0, B, 0, B, B, B, B, B, B, 0, B, 0, B, B, 0, B],
@@ -32,11 +31,7 @@ class Pellet {
 }
 
 let somePellet;
-let foodcounter;
-let B = 2;
-let M = 3;
-let G = 4;
-let state = "start";
+
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -45,23 +40,4 @@ function setup() {
 function draw() {
   background(220);
 
-}
-
-
-
-
-
-// Checking how much food is left // include power pellets
-function foodchecker(){
-  foodcounter = 0;
-  for (let y = 0; y < grid.length; y++) {
-    for (let x = 0; x < grid[y].length; x++) {
-      if (grid[y][x] === 0){
-        foodcounter++;        
-      }
-    }
-  }
-  if (foodcounter === 0){
-    state = "level_one";
-  } 
 }
