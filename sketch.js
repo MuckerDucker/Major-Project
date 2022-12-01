1dd05fdc110646c126bf402b2ad5561ec1db1eef
Kesur0403463 <0403463@sps-student.sk.ca>
2022-12-01 12:17:15 -0600

    added text file and further adapted previous code

--- sketch.js
+++ sketch.js
@@ -3,21 +3,8 @@
 // Novmeber 17, 2022
 
 // used to code ghost movement https://www.todayifoundout.com/index.php/2015/10/ghosts-pac-man-work/#:~:text=Although%20the%20ghosts%20can%20sometimes,turn%20and%20then%20continue%20moving.
-
-let grid = [
-  [B, B, B, B, B, B, B, B, B, B, B, B, B, B, B, B, B, B, B, B],
-  [B, 0, 0, 0, 0, B, 0, 0, 0, 0, 0, 0, 0, 0, B, 0, 0, 0, 0, B],      
-  [B, 0, B, B, 0, B, 0, B, B, B, B, B, B, 0, B, 0, B, B, 0, B],
-  [B, 0, B, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, B, 0, B],
-  [B, 0, B, 0, B, B, 0, B, B, 1, 1, B, B, 0, B, B, 0, B, 0, B],
-  [0, 0, 0, 0, 0, 0, 0, B, G, 1, 1, 1, B, 0, 0, 0, 0, 0, 0, 0],
-  [B, 0, B, 0, B, B, 0, B, B, B, B, B, B, 0, B, B, 0, B, 0, B],
-  [B, 0, B, 0, 0, 0, 0, 0, 0, M, 0, 0, 0, 0, 0, 0, 0, B, 0, B],
-  [B, 0, B, B, 0, B, 0, B, B, B, B, B, B, 0, B, 0, B, B, 0, B],
-  [B, 0, 0, 0, 0, B, 0, 0, 0, 0, 0, 0, 0, 0, B, 0, 0, 0, 0, B],
-  [B, B, B, B, B, B, B, B, B, B, B, B, B, B, B, B, B, B, B, B]];
   
-
+// class definition
 class Pellet {
   constructor(x, y, eaten){
     this.x = x;
@@ -31,11 +18,11 @@ class Pellet {
   }
 }
 
+// defining variables
+let grid = "map.txt";
 let somePellet;
 let foodcounter;
 let B = 2;
-let M = 3;
-let G = 4;
 let state = "start";
 let blockWidth;
 let blockHeight;
@@ -57,12 +44,13 @@ function preload(){
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  grid[pacY][pacX] = M;
-  grid[ghostY][ghostX] = G;
+  // grid[pacY][pacX];
+  // grid[ghostY][ghostX] = G;
 }
 
 function startScreen() {
   background(startImg);
+  // probably text or a start key
 }
 
 function draw() {
@@ -70,6 +58,7 @@ function draw() {
     startScreen();
   }
   if (state === "main"){
+    showPac();
     displayGrid(grid);
     moveWhenSide();
     foodchecker();
@@ -77,200 +66,178 @@ function draw() {
   }
 }
 
-// Checking how much food is left // include power pellets
-function foodchecker(){
-  foodcounter = 0;
-  for (let y = 0; y < grid.length; y++) {
-    for (let x = 0; x < grid[y].length; x++) {
-      if (grid[y][x] === 0){
-        foodcounter++;        
-      }
-    }
-  }
-  if (foodcounter === 0){
-    state = "level_two";
-  } 
-}
-
-function displayGrid(grid) {
-  let blockWidth = width / grid[0].length;
-  let blockHeight = height / grid.length;
-  for (let y = 0; y < grid.length; y++) {
-    for (let x = 0; x < grid[y].length; x++) {
-      if (grid[y][x] === 0) {
-        fill("black");
-        rect(x * blockWidth, y * blockHeight, blockWidth, blockHeight);
-        image(foodImg, x * blockWidth, y * blockHeight, blockWidth, blockHeight);
-      }
-      else if (grid[y][x] === B) {
-        fill("blue");
-        rect(x * blockWidth, y * blockHeight, blockWidth, blockHeight);
-      }
-      else if(grid[y][x] === M){
-        fill("black");
-        rect(x * blockWidth, y * blockHeight, blockWidth, blockHeight);
-        image(pacImg, x * blockWidth, y * blockHeight, blockWidth, blockHeight);
-      }
-      else if(grid[y][x] === G){
-        rect(x * blockWidth, y * blockHeight, blockWidth, blockHeight);
-        image(ghostImg, x * blockWidth, y * blockHeight, blockWidth, blockHeight);
-      }
-      else if(grid[y][x] === 1){
-        fill("black");
-        rect(x * blockWidth, y * blockHeight, blockWidth, blockHeight);
-      }
-    }
-  }
+function showPac(){
+  circle(pacY, pacX, 20);
+  fill("yellow");
 }
 
 
-function keyPressed(){
-  // initiating main screen
-  if (state === "start" && keyCode === 32){
-    state = "main";
-  }
-  // Pac movement
-  if (keyCode === UP_ARROW) {
-    if (grid[pacY-1][pacX] !== B){
-      //sets spot to black(eats food if it exists)
-      grid[pacY][pacX] = 1;
+
+// // Checking how much food is left // include power pellets
+// function foodchecker(){
+//   foodcounter = 0;
+//   for (let y = 0; y < grid.length; y++) {
+//     for (let x = 0; x < grid[y].length; x++) {
+//       if (grid[y][x] === 0){
+//         foodcounter++;        
+//       }
+//     }
+//   }
+//   if (foodcounter === 0){
+//     state = "level_two";
+//   } 
+// }
+
+// function displayGrid(grid) {
+//   let blockWidth = width / grid[0].length;
+//   let blockHeight = height / grid.length;
+//   for (let y = 0; y < grid.length; y++) {
+//     for (let x = 0; x < grid[y].length; x++) {
+//       if (grid[y][x] === 0) {
+//         fill("black");
+//         rect(x * blockWidth, y * blockHeight, blockWidth, blockHeight);
+//         image(foodImg, x * blockWidth, y * blockHeight, blockWidth, blockHeight);
+//       }
+//       else if (grid[y][x] === B) {
+//         fill("blue");
+//         rect(x * blockWidth, y * blockHeight, blockWidth, blockHeight);
+//       }
+//       else if(grid[y][x] === 1){
+//         fill("black");
+//         rect(x * blockWidth, y * blockHeight, blockWidth, blockHeight);
+//       }
+//     }
+//   }
+// }
+
+
+// function keyPressed(){
+//   // initiating main screen
+//   if (state === "start" && keyCode === 32){
+//     state = "main";
+//   }
+//   // Pac movement
+//   if (keyCode === UP_ARROW) {
+//     if (grid[pacY-1][pacX] !== B){
+//       //sets spot to black(eats food if it exists)
+//       grid[pacY][pacX] = 1;
       
-      //move
-      pacY--;
+//       //move
+//       pacY--;
 
-      //changes pacs location
-      grid[pacY][pacX] = M;
-    }
-  }
+//       //changes pacs location
+//       grid[pacY][pacX] = M;
+//     }
+//   }
 
-  if (keyCode === DOWN_ARROW) {
-    if (grid[pacY+1][pacX] !== B) {
-      //setting trail to black
-      grid[pacY][pacX] = 1;
+//   if (keyCode === DOWN_ARROW) {
+//     if (grid[pacY+1][pacX] !== B) {
+//       //setting trail to black
+//       grid[pacY][pacX] = 1;
       
-      //move
-      pacY++;
+//       //move
+//       pacY++;
 
-      //changes pacs location
-      grid[pacY][pacX] = M;
-    }
-  }
+//       //changes pacs location
+//       grid[pacY][pacX] = M;
+//     }
+//   }
 
-  if (keyCode === RIGHT_ARROW) {
-    if (grid[pacY][pacX+1] !== B) {
-      //setting trail to black
-      grid[pacY][pacX] = 1;
+//   if (keyCode === RIGHT_ARROW) {
+//     if (grid[pacY][pacX+1] !== B) {
+//       //setting trail to black
+//       grid[pacY][pacX] = 1;
       
-      //move
-      pacX++;
+//       //move
+//       pacX++;
 
-      //changes pacs location
-      grid[pacY][pacX] = M;
-    }
-  }
+//       //changes pacs location
+//       grid[pacY][pacX] = M;
+//     }
+//   }
 
-  if (keyCode === LEFT_ARROW) {
-    if (grid[pacY][pacX-1] !== B) {
-      //setting trail to black
-      grid[pacY][pacX] = 1;
+//   if (keyCode === LEFT_ARROW) {
+//     if (grid[pacY][pacX-1] !== B) {
+//       //setting trail to black
+//       grid[pacY][pacX] = 1;
       
-      //move
-      pacX-- ;
+//       //move
+//       pacX-- ;
 
-      //changes pacs location
-      grid[pacY][pacX] = M;
-    }
-  }
-
-  // Ghost movement
-  if (keyCode === 87) {
-    if (grid[ghostY-1][ghostX] !== B){
-      // ensuring the ghost leaves no trail
-      if(grid[ghostY-1][ghostX] === 1){
-        grid[ghostY][ghostX] = 1;
-      }
-      if(grid[ghostY-1][ghostX] === 0){
-        grid[ghostY][ghostX] = 0;
-      }
-
-      //move
-      ghostY--;
+//       //changes pacs location
+//       grid[pacY][pacX] = M;
+//     }
+//   }
   
-      //changes ghost's location
-      grid[ghostY][ghostX] = G;
-    }
-  }
+//   if (keyCode === 83) {
+//     if (grid[ghostY+1][ghostX] !== B){
+//       // ensuring the ghost leaves no trail
+//       if(grid[ghostY+1][ghostX] === 1){
+//         grid[ghostY][ghostX] = 1;
+//       }
+//       if(grid[ghostY+1][ghostX] === 0){
+//         grid[ghostY][ghostX] = 0;
+//       }
+//       //move
+//       ghostY++;
   
-  if (keyCode === 83) {
-    if (grid[ghostY+1][ghostX] !== B){
-      // ensuring the ghost leaves no trail
-      if(grid[ghostY+1][ghostX] === 1){
-        grid[ghostY][ghostX] = 1;
-      }
-      if(grid[ghostY+1][ghostX] === 0){
-        grid[ghostY][ghostX] = 0;
-      }
-      //move
-      ghostY++;
+//       //changes ghost's location
+//       grid[ghostY][ghostX] = G;
+//     }
+//   }
   
-      //changes ghost's location
-      grid[ghostY][ghostX] = G;
-    }
-  }
-  
-  if (keyCode === 68) {
-    if (grid[ghostY][ghostX + 1] !== B){
-      // ensuring the ghost leaves no trail
-      if(grid[ghostY][ghostX + 1] === 1){
-        grid[ghostY][ghostX] = 1;
-      }
-      if(grid[ghostY][ghostX + 1] === 0){
-        grid[ghostY][ghostX] = 0;
-      }
+//   if (keyCode === 68) {
+//     if (grid[ghostY][ghostX + 1] !== B){
+//       // ensuring the ghost leaves no trail
+//       if(grid[ghostY][ghostX + 1] === 1){
+//         grid[ghostY][ghostX] = 1;
+//       }
+//       if(grid[ghostY][ghostX + 1] === 0){
+//         grid[ghostY][ghostX] = 0;
+//       }
         
-      //move
-      ghostX++;
+//       //move
+//       ghostX++;
   
-      //changes ghost's location
-      grid[ghostY][ghostX] = G;
-    }
-  }
+//       //changes ghost's location
+//       grid[ghostY][ghostX] = G;
+//     }
+//   }
   
-  if (keyCode === 65) {
-    if (grid[ghostY][ghostX - 1] !== B){
-      // ensuring the ghost leaves no trail
-      if(grid[ghostY][ghostX - 1] === 1){
-        grid[ghostY][ghostX] = 1;
-      }
-      if(grid[ghostY][ghostX - 1] === 0){
-        grid[ghostY][ghostX] = 0;
-      }
+//   if (keyCode === 65) {
+//     if (grid[ghostY][ghostX - 1] !== B){
+//       // ensuring the ghost leaves no trail
+//       if(grid[ghostY][ghostX - 1] === 1){
+//         grid[ghostY][ghostX] = 1;
+//       }
+//       if(grid[ghostY][ghostX - 1] === 0){
+//         grid[ghostY][ghostX] = 0;
+//       }
         
-      //move
-      ghostX-- ;
+//       //move
+//       ghostX-- ;
   
-      //changes ghost's location
-      grid[ghostY][ghostX] = G;
-    }
-  }
-}
+//       //changes ghost's location
+//       grid[ghostY][ghostX] = G;
+//     }
+//   }
+// }
 
+// //Code for Final Project
+// function moveWhenSide(){
+//   if (grid[21][6] && keyCode === RIGHT_ARROW){
+//     pacX = 0;
+//     grid[pacY][pacX] = M;
+//   }
+//   if (grid[0][6] && keyCode === LEFT_ARROW){
+//     pacX = 21;
+//     grid[pacY][pacX] = M;
+//   }
+// }
 
-//Code for Final Project
-function moveWhenSide(){
-  if (grid[21][6] && keyCode === RIGHT_ARROW){
-    pacX = 0;
-    grid[pacY][pacX] = M;
-  }
-  if (grid[0][6] && keyCode === LEFT_ARROW){
-    pacX = 21;
-    grid[pacY][pacX] = M;
-  }
-}
-
-// checking if the ghost won
-function ghostWin(){
-  if(ghostX === pacX && ghostY === pacY){
-    background(ghostWinImg);
-  }
-}
+// // checking if the ghost won
+// function ghostWin(){
+//   if(ghostX === pacX && ghostY === pacY){
+//     background(ghostWinImg);
+//   }
+// }
